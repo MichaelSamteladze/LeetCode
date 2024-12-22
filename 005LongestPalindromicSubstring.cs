8b2823351adfd8d4427251eda4b5622383e88da1
Michael Samteladze <mike.samteladze@gmail.com>
2024-12-22 17:07:19 +0400

    5. Longest Palindromic Substring

---
+++ 005LongestPalindromicSubstring.cs
@@ -0,0 +1,135 @@
+﻿namespace LeetCode._005LongestPalindromicSubstring
+{
+    public class Test
+    {
+        #region Methods
+        public void Solution()
+        {
+            var testString = GetTest3();
+
+            var s = new Solution();
+            var resultString = s.LongestPalindrome(testString);
+            Console.WriteLine(resultString);            
+        }
+
+        string GetTest1()
+        {
+            return "bab";
+        }
+        string GetTest2()
+        {
+            //bb - is longest palindrome
+            return "cbbd";
+        }        
+        string GetTest3()
+        {
+            //amanaplanacanalpanama - is longest palindrome
+            return "madamracecarlevelnoonrotorrefercivicradareveannaabccbahellooliviasawwaswowdeifiedwowpopstepontimeemitnotstepsabacabadabacabaannaayracecarentertennevalerevellevelamanaplanacanalpanamamoonrotorrotootnursesrunnittenitetimsaptopsstoptimsapevilolivepopneveroddorevenabacabadabacababobdaddadhannahannahlevelmadamtacocatpopstatsgoogcivicradardeifiedrotorrefernoonracecarredrummurderstepontimeemitnotstepsnitinamanaplanacanalpanamamadamkayakpopdeifiedradarevellevelmadamrotorreferradarracecarnooncivicmadamgoogwowreferradarmadamcivicwowkayaklevelrotorreferannaamanaplanacanalpanamaeveannahannahmoonrotorwowdeifiedlevelcivicstatswowkayakradarrotorracecarpopgoogwowstepontimeemitnotsteps";
+        }
+        string GetTest4()
+        {
+            return "a";
+        }
+        #endregion
+    }
+    
+    public class Solution
+    {
+        #region Methods
+        public string LongestPalindrome(string s)
+        {
+            var perfectStart = 0;
+            var maxLength = 1;
+
+            var l = s.Length;
+
+            for (var i = 0; i < l; i++)
+            {
+                for (var j = i; j < l; j++)
+                {
+                    var length = j - i + 1;
+
+                    var isPalindrome = IsPalindrome(s, i, length);
+                    if (isPalindrome)
+                    {                        
+                        if(length > maxLength)
+                        {
+                            perfectStart = i;
+                            maxLength = length;                            
+                        }                        
+                    }
+                }
+            }
+
+            return s.Substring(perfectStart, maxLength);
+        }
+
+        public bool IsPalindrome(string s, int start, int length)
+        {
+            var IsPalindrome = true;
+
+            var end = start + length;
+            var half = length / 2;
+            var iterationNumber = 0;
+            for (var i = start; i < start + half; i++)
+            {
+                IsPalindrome = s[i]! == s[end - (++iterationNumber)];
+                if (!IsPalindrome)
+                {
+                    break;
+                }
+            }
+            return IsPalindrome;
+        }
+        #endregion
+    }
+
+    public class SolutionWithSpans
+    {
+        #region Methods
+        public string LongestPalindrome(string s)
+        {
+            var resultSpan = default(ReadOnlySpan<char>);
+            var maxLength = 0;
+
+            var l = s.Length;
+
+            for (var i = 0; i < l; i++)
+            {
+                for (var j = i; j < l; j++)
+                {
+                    var testSpan = s.AsSpan(i, j - i + 1);
+                    var isPalindrome = IsPalindrome(testSpan);
+                    if (isPalindrome)
+                    {
+                        var palindromeLength = testSpan.Length;
+                        if (palindromeLength > maxLength)
+                        {
+                            maxLength = palindromeLength;
+                            resultSpan = testSpan;
+                        }
+                    }
+                }
+            }
+
+            return resultSpan.ToString();
+        }
+
+        bool IsPalindrome(ReadOnlySpan<char> stringSpan)
+        {
+            var IsPalindrome = true;
+            var l = stringSpan.Length;
+            var half = l / 2;
+            for (var i = 0; i < half; i++)
+            {
+                IsPalindrome = stringSpan[i]! == stringSpan[l - 1 - i];
+                if (!IsPalindrome)
+                {
+                    break;
+                }
+            }
+            return IsPalindrome;
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@
             //new LeetCode._001TwoSum.Test().Solution();
             //new LeetCode._002AddTwoNumbers.Test().Solution();
             //new LeetCode._003LongestSubstringWithoutRepeatingCharacters.Test().Solution();
-            new LeetCode._004MedianOfTwoSortedArrays.Test().Solution();
-            
+            //new LeetCode._004MedianOfTwoSortedArrays.Test().Solution();
+            new LeetCode._005LongestPalindromicSubstring.Test().Solution();
+
         }
     }
 }
