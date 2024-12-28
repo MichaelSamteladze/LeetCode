d72b858fc3f013a6e35874492424ac0c106af452
Michael Samteladze <mike.samteladze@gmail.com>
2024-12-28 18:28:44 +0400

    8. String to Integer (atoi)

---
+++ 008StringToIntegerATOI.cs
@@ -0,0 +1,97 @@
+﻿namespace LeetCode._008StringToIntegerATOI
+{
+    public class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            var result = 0;
+            var l = s.Length;            
+            var position = 0;
+
+            if (l > 0)
+            {
+                var c = default(char);
+                var shouldSkip = true;
+                while (shouldSkip)
+                {
+                    c = s[position];
+                    shouldSkip = (c == ' ') && (++position < l);                    
+                }
+
+                if (position < l)
+                {
+                    c = s[position];
+                    var isNegative = false;
+                    if (c == '-')
+                    {
+                        isNegative = true;
+                        ++position;
+                    }
+                    else if (c == '+')
+                    {
+                        ++position;
+                    }
+
+                    if (position < l)
+                    {
+                        c = s[position];
+                        var shouldContinue = (c > 47 && c < 58);
+
+                        while (shouldContinue)
+                        {
+                            result *= 10;
+                            if (result % 10 == 0)
+                            {
+                                var numberToAdd = (c - 48) * (isNegative ? -1 : 1);
+
+                                var shouldAdd = isNegative ? int.MinValue - result - numberToAdd < 0 : int.MaxValue - result - numberToAdd > 0;
+
+                                if (shouldAdd)
+                                {
+                                    result += numberToAdd;
+                                }
+                                else
+                                {
+                                    if (isNegative)
+                                    {
+                                        result = int.MinValue;
+                                    }
+                                    else
+                                    {
+                                        result = int.MaxValue;
+                                    }
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                if (isNegative)
+                                {
+                                    result = int.MinValue;
+                                }
+                                else
+                                {
+                                    result = int.MaxValue;
+                                }
+                                break;                                
+                            }
+
+                             ++position;
+                            if(position < l)
+                            {
+                                c = s[position];
+                                shouldContinue = (c > 47 && c < 58);
+                            }
+                            else
+                            {
+                                shouldContinue = false;
+                            }
+                        }
+                    }
+                }                
+            }
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            var solution = new LeetCode._008StringToIntegerATOI.Solution();
+            //var result = solution.MyAtoi("   00354");
+            //Console.WriteLine(result);
+            Console.WriteLine(int.MinValue);
         }
     }
 }
